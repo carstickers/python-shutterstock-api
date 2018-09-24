2f655f99f56dd12a0d91b5a6b1d29bb48c2bf47c
Matt <matt@carstickers.com>
2018-09-24 08:40:47 -0700

    Fixed typ in description

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
   name='shutterstock',
   packages=list(find_packages(shutterstock.__path__, shutterstock.__name__)),
   version='0.1.1',
-  description='Pyhton Shutterstock API Client',
+  description='Python Shutterstock API Client',
   author='Matt Roberts',
   author_email='contact@maleero.com',
   url='https://github.com/malero/python-shutterstock-api',
