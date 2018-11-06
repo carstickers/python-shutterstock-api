05b754fc07976b473649a32b1217827b1cbd9e07
Matt <contact@maleero.com>
2018-11-06 14:29:41 -0800

    Version++

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ def find_packages(path, prefix=""):
 setup(
   name='shutterstock',
   packages=list(find_packages(shutterstock.__path__, shutterstock.__name__)),
-  version='0.1.3',
+  version='0.1.4',
   description='Python Shutterstock API Client',
   author='Matt Roberts',
   author_email='contact@maleero.com',
   url='https://github.com/malero/python-shutterstock-api',
-  download_url='https://github.com/malero/python-shutterstock-api/archive/v0.1.3.tar.gz',
+  download_url='https://github.com/malero/python-shutterstock-api/archive/v0.1.4.tar.gz',
   keywords=['shutterstock', ],
   classifiers=[],
 )
