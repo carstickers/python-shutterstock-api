9551680001260affa6832297cd369e92ef13645a
blayzen-w <blayze@carstickers.com>
2019-07-10 15:38:43 -0700

    Removed content from exception

--- shutterstock/exceptions.py
+++ shutterstock/exceptions.py
@@ -2,12 +2,10 @@
 class APIError(Exception):
     message = None
     status_code = None
-    content = None
 
-    def __init__(self, message, status_code, content):
+    def __init__(self, message, status_code):
         self.message = message
         self.status_code = status_code
-        self.content = content
 
 
 class APIRedirectError(APIError):
